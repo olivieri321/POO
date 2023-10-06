dff50238b335b427ab149e70046f529e5da54669
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-10-06 17:01:01 -0300

    Delete TP2/ejercicioSistemaPrendas/Practica directory

--- TP2/ejercicioSistemaPrendas/Practica/src/com/ejercicio/unlu/Tarjeta.java
+++
@@ -1,7 +0,0 @@
-package com.ejercicio.unlu;
-
-public abstract class Tarjeta {
-    public double aplicarDescuento(double monto){
-        return monto * 0.99;
-    }
-}
--- TP2/ejercicioSistemaPrendas/Practica/src/com/ejercicio/unlu/TarjetaDorada.java
+++
@@ -1,8 +0,0 @@
-package com.ejercicio.unlu;
-
-public class TarjetaDorada extends Tarjeta {
-    @Override
-    public double aplicarDescuento(double monto) {
-        return monto*0.995;
-    }
-}
